bfa2bd2b452d1fc8abdd9220b3b9cf2bfba5c451
SukiYuSijing <15767301655@163.com>
2023-12-17 14:10:44 +0800

    多选自动计算长度组件

--- src/components/multi-select.vue
+++ src/components/multi-select.vue
@@ -57,6 +57,7 @@ function useObserver(el: Element) {
   observer.observe(el, config)
   return observer
 }
+let contradict = false
 function useTagsChangesObserver(wrapperEle: Element, immediate = false) {
   const config = {
     childList: true,
@@ -77,13 +78,23 @@ function useTagsChangesObserver(wrapperEle: Element, immediate = false) {
     }
     const preChildren: HTMLElement = children[0]
     const lastChildren: HTMLElement = children[children.length - 1]
+    const wrapperWidth = wrapperEle.parentElement.clientWidth
+
     if (
-      currentLen > previousLen &&
-      lastChildren.offsetTop > preChildren.offsetTop
+      currentLen > previousLen ||
+      lastChildren.offsetTop > preChildren.offsetTop ||
+      lastChildren.offsetLeft + lastChildren.clientWidth + props.inputEleWidth >
+        wrapperWidth
     ) {
       for (let i = children.length - 1; i > 0; i--) {
         const currentChild: HTMLElement = children[i]
-        if (currentChild.offsetTop == preChildren.offsetTop) {
+        if (
+          currentChild.offsetTop == preChildren.offsetTop &&
+          currentChild.offsetLeft +
+            currentChild.clientWidth +
+            props.inputEleWidth <=
+            wrapperWidth
+        ) {
           nextTick(() => {
             maxCollapseTags.value = i
           })
@@ -95,18 +106,24 @@ function useTagsChangesObserver(wrapperEle: Element, immediate = false) {
       const inputElementWidth = inputElement?.clientWidth || 0
       nextTick(() => {
         if (!inputElement || inputElementWidth > props.inputEleWidth) {
-          if (currentLen > maxCollapseTags.value)
+          if (!contradict && currentLen > maxCollapseTags.value)
             //需要解决，新增的标签过长，又重新占满了input的空间的问题
             //标签最大宽度应该和el-select的width减去input的宽度
             // resizeObserver通过方式替代，否则太多的observe
             maxCollapseTags.value = maxCollapseTags.value + 10
-        } else maxCollapseTags.value = children.length - 1
+        } else {
+          maxCollapseTags.value = children.length - 1
+          contradict = true
+        }
       })
     }
     previousLen = currentLen
     setTimeout(() => {
       calculating.value = false
     }, 0)
+    setTimeout(() => {
+      contradict = false
+    }, 1000)
   }
   if (immediate) {
     callback()
