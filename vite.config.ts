b297196d3cc51805af576a474740a52eb7aec027
SukiYuSijing <15767301655@163.com>
2023-12-19 05:45:06 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (format) => {
         return `multi-select.${format}.js`
       },
-      formats: ['cjs', 'es', 'umd'],
+      formats: ['es'],
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
